5e42c134395c67382afe184e1bae222f9d439542
Semion <sfurlender@gmail.com>
2023-01-30 10:48:16 +0200

    .

---
+++ src/components/generateButton/GenerateButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+
+export const GenerateButton = (props) => {
+  const connectToServer = () => {};
+  return (
+    <div>
+      <button onClick={connectToServer}></button>
+    </div>
+  );
+};
